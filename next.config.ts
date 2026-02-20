d2cd2a7f6e9f177aa437e08f172dfbb205ed8c8c
suyogstha7 <suyogstha317@gmail.com>
2026-02-20 11:47:36 +0545

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["jsdom", "html-encoding-sniffer"],
-  transpilePackages: ["@exodus/bytes"],
+  transpilePackages: ["@exodus/bytes", "html-encoding-sniffer", "whatwg-url", "abab"],
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
-      { protocol: "https", hostname: "sapi.travelnepal.com.au" },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "sapi.travelnepal.com.au",
+      },
       {
         protocol: "http",
         hostname: "192.168.254.46",
@@ -19,26 +25,58 @@ const nextConfig: NextConfig = {
         port: "8002",
         pathname: "/media/**",
       },
-      { protocol: "http", hostname: "192.168.1.81", port: "8000" },
-      { protocol: "http", hostname: "192.168.254.30", port: "8002" },
-      { protocol: "http", hostname: "192.168.1.64", pathname: "/**" },
-      { protocol: "http", hostname: "192.168.1.94", pathname: "/**" },
-      { protocol: "http", hostname: "192.168.1.104", pathname: "/**" },
+      {
+        protocol: "http",
+        hostname: "192.168.1.81",
+        port: "8000",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.254.30",
+        port: "8002",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.1.64",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.1.94",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.1.104",
+        pathname: "/**",
+      },
       {
         protocol: "https",
         hostname: "192.168.1.64",
         port: "8000",
         pathname: "/**",
       },
-      { protocol: "http", hostname: "api.mayaramasala.com", pathname: "/**" },
-      { protocol: "http", hostname: "192.168.1.68", pathname: "/**" },
+      {
+        protocol: "http",
+        hostname: "api.mayaramasala.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.1.68",
+        pathname: "/**",
+      },
       {
         protocol: "https",
         hostname: "192.168.1.68",
         port: "8000",
         pathname: "/**",
       },
-      { protocol: "http", hostname: "192.168.1.97", pathname: "/**" },
+      {
+        protocol: "http",
+        hostname: "192.168.1.97",
+        pathname: "/**",
+      },
       {
         protocol: "https",
         hostname: "192.168.1.97",
@@ -63,17 +101,37 @@ const nextConfig: NextConfig = {
         port: "8002",
         pathname: "/media/**",
       },
-      { protocol: "http", hostname: "api.careegram.com", pathname: "/**" },
-      { protocol: "https", hostname: "api.careegram.com", pathname: "/**" },
+      {
+        protocol: "http",
+        hostname: "api.careegram.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.careegram.com",
+        pathname: "/**",
+      },
       {
         protocol: "http",
         hostname: "192.168.1.126",
         port: "8001",
         pathname: "/media/**",
       },
-      { protocol: "https", hostname: "images.unsplash.com", pathname: "**" },
-      { protocol: "https", hostname: "i.pravatar.cc", pathname: "**" },
-      { protocol: "https", hostname: "illustrations.popsy.co", pathname: "**" },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "illustrations.popsy.co",
+        pathname: "**",
+      },
     ],
   },
   output: "standalone",
